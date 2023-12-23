eaa7bd775799b5e47fc19b597f41e59c3ac61797
Hashen-Janu <paranamanajanu@gmail.com>
2023-12-23 22:03:41 +0530

    first commit

---
+++ ecoms/src/main/java/com/januOfficial/ecoms/EcomsApplication.java
@@ -0,0 +1,13 @@
+package com.januOfficial.ecoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomsApplication.class, args);
+	}
+
+}
